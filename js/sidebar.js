96163a85ac8924c3e50d4f3e5e3bcca2ce158db8
mrnotbadguy <dmitrysafronov1996@gmail.com>
2018-11-12 21:23:21 +0300

    Generate sidebar list by js script

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,42 @@
+var carea = document.getElementById('center-area'),
+    larea = document.getElementById('left-area'),
+    rarea = document.getElementById('right-area');
+
 document.addEventListener('DOMContentLoaded', function() {
+    doListInSidebar();
+    doFadeOutSidebarElement();
+});
+
+function doListInSidebar()
+{
+    const styleClass = 'sidebar-elements-style';
+    var sidebar = document.getElementById('sidebar');
+    var element = null;
+
+    // На главную
+    element = document.createElement('div');
+    element.setAttribute('id', 'main-page');
+    element.setAttribute('class', styleClass);
+    // Добавить создание ссылки
+    element.innerText = 'Главная';
+    sidebar.appendChild(element);
+
+    // О сайте
+    element = document.createElement('div');
+    element.setAttribute('id', 'about-page');
+    element.setAttribute('class', styleClass);
+    element.innerText = 'О сайте';
+    sidebar.appendChild(element);
+}
+
+function doFadeOutSidebarElement()
+{
     var link = window.location.pathname;
     if (link == '/' || link == '/index.html')
     {
         document.getElementById('main-page').style.background = '#CCC';
     }
-});
-
-var carea = document.getElementById('center-area'),
-    larea = document.getElementById('left-area'),
-    rarea = document.getElementById('right-area');
+}
 
 window.onresize = function(e) {
     const _width = 770;    
